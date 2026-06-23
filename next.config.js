4cd478d2b8c9f6c4a0dd730697cc1e9021ecb21e
Piotr Baran <fhubaran@op.pl>
2026-06-23 22:36:35 +0200

    RC-005: trace only required CRM workbook

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
   reactStrictMode: true,
   outputFileTracingRoot: require('path').join(__dirname),
   outputFileTracingIncludes: {
-    '/*': ['./crm/**/*'],
+    '/*': ['./crm/ADEZO_OS_2_0_BUILD034K_CRM_CLEANUP_FINAL_NEW.xlsx'],
   },
 };
